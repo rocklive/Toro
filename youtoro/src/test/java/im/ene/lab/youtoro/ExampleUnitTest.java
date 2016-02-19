62a68ef04363e216e84b7fe0549ca91da54ad223
Nguyen Hoai Nam <mr.nguyenhoainam@gmail.com>
2016-02-19 11:19:03 +0900

    Add build folder to ignore. Change a local package/folder name

--- youtoro/src/test/java/im/ene/lab/youtoro/ExampleUnitTest.java
+++ youtoro/src/test/java/im/ene/lab/youtoro/ExampleUnitTest.java
@@ -14,16 +14,17 @@
  * limitations under the License.
  */
 
-package im.ene.lab.torox;
+package im.ene.lab.youtoro;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-  public ApplicationTest() {
-    super(Application.class);
+public class ExampleUnitTest {
+  @Test public void addition_isCorrect() throws Exception {
+    assertEquals(4, 2 + 2);
   }
 }
